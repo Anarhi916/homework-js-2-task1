1492004baab2bb42915c44fb502f60e9c49108d0
Andrey Filimonov <gipget@mail.ru>
2017-07-14 19:03:22 +0300

    corrected

--- js.js
+++ js.js
@@ -8,7 +8,12 @@ function howOld(){
         alert('Ваш возвраст: ' + (2017 - input.value) + ' лет');
     }
     else if(input.value.length === 2){
-        alert('Ваш возвраст: ' + (17 - input.value));
+          if(input.value >= 0 && input.value < 18){
+              alert('Ваш возвраст: ' + (17 - input.value));
+          }
+          else{
+              alert('Ваш возвраст: ' + (2017 - ('19' + input.value)));
+          }  
     }   
     else{
         alert('Введите корректную дату рождения');
