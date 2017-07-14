ec813f8e47b47b0744a7ae51b0af7c8894a84bb2
Andrey Filimonov <gipget@mail.ru>
2017-07-14 20:53:15 +0300

    a

--- js.js
+++ js.js
@@ -3,19 +3,33 @@ let button = document.querySelector('button');
 
 button.addEventListener('click', howOld);
 
-function howOld(){
-    if(input.value.length === 4){
-        alert('Ваш возвраст: ' + (2017 - input.value) + ' лет');
+// function howOld(){
+//     if(input.value.length === 4){
+//         alert('Ваш возвраст: ' + (2017 - input.value) + ' лет');
+//     }
+//     else if(input.value.length === 2){
+//           if(input.value >= 0 && input.value < 18){
+//               alert('Ваш возвраст: ' + (17 - input.value));
+//           }
+//           else{
+//               alert('Ваш возвраст: ' + (2017 - ('19' + input.value)));
+//           }  
+//     }   
+//     else{
+//         alert('Введите корректную дату рождения');
+//     } 
+// }
+
+function howOld(){    
+    const now= new Date();    
+    const userDate = new Date((parseInt(input.value, 10)), 01, 01);    
+    if(input.value >= 0 && input.value < 18){
+         alert('Ваш возраст: ' + (now.getFullYear() - (2000 + parseInt(input.value, 10))));
     }
-    else if(input.value.length === 2){
-          if(input.value >= 0 && input.value < 18){
-              alert('Ваш возвраст: ' + (17 - input.value));
-          }
-          else{
-              alert('Ваш возвраст: ' + (2017 - ('19' + input.value)));
-          }  
-    }   
     else{
-        alert('Введите корректную дату рождения');
-    } 
-}
+        alert('Ваш возраст: ' + (now.getFullYear() - userDate.getFullYear()));
+    }
+}    
+
+
+
